799f66bb32fec24e9f62738e250000fad5a16c20
tanya0394@mail.ru <Wine4My_lo>
2018-07-01 14:40:46 +0300

    Added logic for checking winner.

--- src/main/java/com/example/TicTacToe.java
+++ src/main/java/com/example/TicTacToe.java
@@ -1,37 +1,85 @@
 package com.example;
 
+// 1='X'  2='O'
 public class TicTacToe {
     private int dimension;
     private LogicImpl opponent;
-    public TicTacToe(int dimension, LogicImpl opponent) {
 
+    public TicTacToe(int dimension, LogicImpl opponent) {
+        this.dimension = dimension;
+        this.opponent = opponent;
     }
 
-    public char getWinner(Board board){
-        checkHorizontal(board);
-        checkVertical(board);
-        checkDiagonal(board);
+    public char getWinner(Board board) {
+        int result;
+        result = checkHorizontal(board);
+        if (result > 0) return board.getBoard()[result] == 1 ? 'X' : 'O';
+        result = checkVertical(board);
+        if (result > 0) return board.getBoard()[result] == 1 ? 'X' : 'O';
+        result = checkDiagonal(board);
+        if (result > 0) return board.getBoard()[result] == 1 ? 'X' : 'O';
         return ' ';
     }
 
-    private void checkHorizontal(Board board) {
-        int lastIndex = 1;
-        while (lastIndex < dimension){
-            if (board.getBoard()[0] != board.getBoard()[lastIndex]){
+    private int checkOneCase(int condition, int firstIndex, int lastIndex, Board board, int condForLastIndex) {
+        int result = -10;
+        while (lastIndex <= condition) {
+            if (board.getBoard()[firstIndex] != board.getBoard()[lastIndex]) {
+                result = -1;
                 break;
-            }
-            else {
-                lastIndex++;
+            } else {
+                lastIndex = lastIndex + condForLastIndex;
             }
         }
+        return result;
     }
 
-    private void checkVertical(Board board) {
+    private int checkHorizontal(Board board) {
+        //initial result for checking
+        int result = -10;
+        for (int firstIndex = 0; firstIndex < dimension; firstIndex++) {
 
+            result = checkOneCase(dimension - 1, firstIndex, firstIndex + 1, board, 1);
+
+            if (result == -10) {
+                result = board.getBoard()[firstIndex];
+                break;
+            }
+        }
+        return result;
     }
 
-    private void checkDiagonal(Board board) {
+    private int checkVertical(Board board) {
+        int result = -10;
+        int condition = dimension * (dimension - 1);
+        for (int firstIndex = 0; firstIndex < dimension; firstIndex++) {
 
+            result = checkOneCase(condition, firstIndex, dimension, board, dimension);
+
+            if (result == -10) {
+                result = board.getBoard()[firstIndex];
+                break;
+            }
+        }
+        return result;
+    }
+
+    private int checkDiagonal(Board board) {
+        int result;
+        //first diagonal
+        int firstIndex = 0;
+        int condition = dimension * dimension - 1;
+        result = checkOneCase(condition, firstIndex, dimension + 1, board, dimension + 1);
+
+        //check second diagonal if first didn't find winner
+        if (result == -1) {
+            firstIndex = dimension - 1;
+            condition = dimension * (dimension - 1);
+            result = checkOneCase(condition, firstIndex, firstIndex + (dimension - 1), board, dimension - 1);
+        }
+        if (result == -10) result = board.getBoard()[firstIndex];
+
+        return result;
     }
 
 
